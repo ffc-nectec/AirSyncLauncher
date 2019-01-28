fbcc11a265d73b0caa4e1c807fa2c3e26ddecede
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-01-28 15:29:12 +0700

    update and run airsync

--- src/main/kotlin/launcher/Main.kt
+++ src/main/kotlin/launcher/Main.kt
@@ -1,15 +1,27 @@
 package launcher
 
+import launcher.zip.DownloadZipStream
 import version.apigithub.ApiGithubRelease
+import java.net.URL
 
 private const val commandGetAirsyncVersion = "java -jar airsync.jar -v"
+private const val commandRunAirsync = "cmd /k start java -jar airsync.jar"
 
 internal class Main constructor(val args: Array<String>) {
     fun run() {
         val proc = Runtime.getRuntime().exec(commandGetAirsyncVersion)
         val airsyncVersion = proc.inputStream.reader().readText()
-        val githubRelease = ApiGithubRelease("ffc-nectec/airsync").getLastRelease()
+        val github = ApiGithubRelease("ffc-nectec/airsync").getLastRelease()
+        if (airsyncVersion != github.tag_name) {
+            val assertInstall = github.assets.find { it.name == "install.zip" }!!
+            val urlZip = URL(assertInstall.browser_download_url)
+            val zip = DownloadZipStream(urlZip, assertInstall.size)
+
+            zip.download()
+        }
+
         println(airsyncVersion)
+        Runtime.getRuntime().exec(commandRunAirsync)
     }
 }
 
