24c6aaccd8d969943772d93c05e5dc9c6b6fc66a
Thanachai Thongkum X1 Carbon2017 <singmax161@gmail.com>
2019-02-06 16:29:24 +0700

    run diff x86 x64

--- src/main/kotlin/launcher/Main.kt
+++ src/main/kotlin/launcher/Main.kt
@@ -2,13 +2,15 @@ package launcher
 
 import launcher.view.PrintView
 import launcher.zip.DownloadZipStream
+import max.java.c64support.CheckJava64BitSupportWithCommand
 import max.kotlin.checkdupp.CheckDupplicateWithRest
 import max.kotlin.checkdupp.DupplicateProcessException
 import version.apigithub.ApiGithubRelease
 import java.net.URL
 
 private const val commandGetAirsyncVersion = "java -jar airsync.jar -v"
-private const val commandRunAirsync = "cmd /k start java -Xms1G -Xmx4G -jar -Dfile.encoding=UTF-8 -jar airsync.jar"
+private const val defaultRunAirsync = "cmd /k start java -Xms1G -Xmx3G -jar -Dfile.encoding=UTF-8 -jar airsync.jar"
+private const val x64RunAirsync = "cmd /k start java -d64 -Xms1G -Xmx4G -jar -Dfile.encoding=UTF-8 -jar airsync.jar"
 
 internal class Main constructor(val args: Array<String>) {
     private val procName = CheckDupplicateWithRest("airsync")
@@ -59,7 +61,10 @@ internal class Main constructor(val args: Array<String>) {
         }
         pv.append = "Complete patch..."
         println(airsyncVersion)
-        Runtime.getRuntime().exec(commandRunAirsync)
+        if (CheckJava64BitSupportWithCommand().is64Support())
+            Runtime.getRuntime().exec(x64RunAirsync)
+        else
+            Runtime.getRuntime().exec(defaultRunAirsync)
         pv.dispose()
         System.exit(0)
     }
