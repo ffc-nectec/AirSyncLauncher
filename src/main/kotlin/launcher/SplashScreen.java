924d507f4f73315f60599c84159728964a62eb38
Piruin Panichphol <piruin.p@gmail.com>
2019-03-20 14:48:10 +0700

    Add UI Component

---
+++ src/main/kotlin/launcher/SplashScreen.java
@@ -0,0 +1,10 @@
+package launcher;
+
+import javax.swing.*;
+
+public class SplashScreen {
+    public JPanel panel1;
+    public JProgressBar progressBar1;
+
+
+}
---
+++ src/main/kotlin/launcher/Ui.kt
@@ -0,0 +1,7 @@
+package launcher
+
+interface Ui {
+    fun show()
+    var text: String
+    fun dispose()
+}
